85e3141e833bf4399be3b7ea8a60c9bdc498b69c
Adeyemi Afeez <tbalogun27@gmail.com>
2022-09-15 12:19:27 -0700

    first task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
-nclude "main.h"
+#include "main.h"
 /**
- *  main  prints putchar
+ *  main - prints _putchar followed by new line
  *
- *  Return: always 0
+ *  Return: 0 (success)
  */
+
 int main(void)
 {
 _putchar('_');
@@ -14,6 +15,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('\n);
+_putchar('\n');
 return (0);
 }
