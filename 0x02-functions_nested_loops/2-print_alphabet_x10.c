1e2ff10e9fb835f659fae3f9cf225eb591c6cecf
Adeyemi Afeez <tbalogun27@gmail.com>
2022-09-16 09:48:21 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *   print_alphabet_x10 - repeats print_alphabet 10 times
+ *   Return: Nothing
+ */
+void print_alphabet_x10(void)
+}
+char c, i;
+for (i = 0; i <= 9; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
