cbe17d993e88927ef56f1b8e6f1c6f465587cbfb
Adeyemi Afeez <tbalogun27@gmail.com>
2022-09-18 14:23:53 -0700

    Cotrection

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *   islower - Program to check lowercase character
+ *   _islower - Program to check lowercase character
  *   @c: integer variable to be check
  *   Return: 1 if it is lowercase and 0 if it is not a lowercase
  */
