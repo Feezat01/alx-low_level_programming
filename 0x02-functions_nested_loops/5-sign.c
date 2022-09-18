889035250624c464e1ef854e7bffcc5cd5be22fc
Adeyemi Afeez <tbalogun27@gmail.com>
2022-09-18 14:57:05 -0700

    Sign in

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
  *  print_sign - function that prints the sign of a number.
  *  @n: the number to be checked.
@@ -12,14 +12,14 @@ if (n > 0)
 _putchar('+');
 return (1);
 }
-else if (n == 0)
-{
-_putchar('0');
-return (0);
-}
-else
+else if (n < 0)
 {
 _putchar('-');
 return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
 }
 }
